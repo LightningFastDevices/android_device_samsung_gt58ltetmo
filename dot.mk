eb88d2730e26dbf1422c385e42a7bbd7f284fd46
Vincent Zvikaramba <zvikaram@teach.cs.toronto.edu>
2018-10-07 01:11:53 -0400

    gt58ltetmo: Add dotOS Makefile
    Change-Id: Ie9ade0de9d666c6339c18186b1ef0bd9068894aa

---
+++ dot.mk
@@ -0,0 +1,18 @@
+# Inherit from common
+$(call inherit-product, device/samsung/gte-common/dot.mk)
+
+$(call inherit-product, device/samsung/gt58ltetmo/device.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1024
+TARGET_SCREEN_WIDTH := 768
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := gt58ltetmo
+PRODUCT_NAME := dot_gt58ltetmo
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-T357T
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_CHARACTERISTICS := tablet
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
