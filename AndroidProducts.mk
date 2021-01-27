6ed8ad2cb5866194da541fcf83624e71a9e717d7
zeeshan <itexpert120@gmail.com>
2021-01-27 20:21:19 +0500

    new lunch choices

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 LOCAL_PATH := device/samsung/gt58ltetmo
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/lineage_gt58ltetmo.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_gt58ltetmo-eng \
+    lineage_gt58ltetmo-userdebug \
+    lineage_gt58ltetmo-user
